dd31e4ca4b92366d5bbfe5a96adb8a10a1c6c600
PushkarDS <118673642+PushkarDS@users.noreply.github.com>
2024-11-05 10:20:33 +0530

    Update userAction.js
    url

--- src/actions/userAction.js
+++ src/actions/userAction.js
@@ -37,7 +37,7 @@ import {
   CLEAR_ERRORS,
 } from "../constants/userConstants";
 import axios from "axios";
-axios.defaults.baseURL = "http://localhost:4000/api/v1";
+axios.defaults.baseURL = "https://ecommerce-backend-2uy5.onrender.com/api/v1";
 
 // Login
 export const login = (email, password) => async (dispatch) => {
